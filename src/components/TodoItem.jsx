0603a08b6920140e90db5c09159dafca6406b0e9
Rey Perez <reyfpd@gmail.com>
2024-01-26 00:17:01 -0400

    change markup of TodoList and TodoItems to ul and li

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,23 +4,22 @@ import { TodoContext } from "./Todo";
 export default function TodoItem({item}){
     
     const [checked, setChecked] = useState(false);
-    const deleteTodo = useContext(TodoContext)
+    const context = useContext(TodoContext)
 
     function handleChange(){
         setChecked(!checked);
     };
 
     return(
-        <div className="todo-item">
+        <li className="todo-item">
             <input type="checkbox" onChange={handleChange}/>
             <span>{!checked? item.title : <s>{item.title}</s>}</span>
-            {/* <button className="button" onClick={()=>deleteTodo(item)}><img src={Trash} alt="" /></button> */}
-            <button className="button" onClick={()=>deleteTodo(item)}>
+            <button className="button" onClick={()=>context.deleteTodo(item)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                 </svg>
             </button>
-        </div>
+        </li>
         
     );
 }
