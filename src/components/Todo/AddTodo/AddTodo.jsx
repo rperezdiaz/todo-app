fbb8ba7b50cf3026a98ec9965fda0ddf23918587
Rey Perez <reyfpd@gmail.com>
2024-01-29 00:43:57 -0400

    Show delete icon only when hovering task

--- src/components/Todo/AddTodo/AddTodo.jsx
+++ src/components/Todo/AddTodo/AddTodo.jsx
@@ -19,7 +19,7 @@ export default function AddTodo(){
     return(
         <form action="" onSubmit={handleSubmit}>
             <input ref={todoTitle} id='add-form-input' className="add-form" type="text" placeholder="ADD NEW TASK"/>
-            <button id="add-form-button" className="add-form">
+            <button id="add-form-button" className="add-form icon-container">
                 <Icon.Add />
             </button>
         </form>
--- src/components/Todo/TodoItem/TodoItem.jsx
+++ src/components/Todo/TodoItem/TodoItem.jsx
@@ -4,6 +4,7 @@ import { Icon } from "../../Icon";
 
 export default function TodoItem({item}){
     
+    const [hovering, setHovering] = useState(false)
     const [checked, setChecked] = useState(false);
     const context = useContext(TodoContext)
 
@@ -11,8 +12,18 @@ export default function TodoItem({item}){
         setChecked(!checked);
     };
 
+    const handleMouseEnter = () => {
+        setHovering(true);
+    }
+    const handleMouseLeave = () => {
+        setHovering(false);
+    }
+
     return(
-        <li className="todo-item-container" >
+        <li 
+        className="todo-item-container" 
+        onMouseEnter={handleMouseEnter} 
+        onMouseLeave={handleMouseLeave}>
             <div className="checkbox-title-container" onClick={handleChange}>
                 <input type="checkbox" className="checkbox" checked={checked} />
                 <span className="todo-title">{
@@ -20,11 +31,12 @@ export default function TodoItem({item}){
                     </span>
             </div>
             
-            <button className="delete-todo" onClick={()=>context.deleteTodo(item)}>
+            <button className={`delete-todo icon-container`} onClick={()=>context.deleteTodo(item)}>
                 {/* <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                 </svg> */}
-                <Icon.Trashcan />
+                { hovering ? <Icon.Trashcan />: null}
+                
             </button>
         </li>
         
