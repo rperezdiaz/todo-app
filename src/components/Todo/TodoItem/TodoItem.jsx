8e4bddc06810653fb95ef1a33cd02b04d287411a
Rey Perez <reyfpd@gmail.com>
2024-01-30 17:18:03 -0400

    add edit form when doubleclick todo title

--- src/components/Todo/TodoItem/TodoItem.jsx
+++ src/components/Todo/TodoItem/TodoItem.jsx
@@ -1,11 +1,13 @@
-import { useState, useContext, useReducer} from "react";
+import { useState, useContext, useReducer, useRef} from "react";
 import { TodoContext } from "../Todo";
 import { Icon } from "../../Icon";
 
 export default function TodoItem({item}){
     const [todo ,dispatch] = useReducer(todoItemReducer, item);
     const [hovering, setHovering] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
     const context = useContext(TodoContext);
+    const editedTitle = useRef()
 
     const handleChange = () =>{
         dispatch({
@@ -14,7 +16,20 @@ export default function TodoItem({item}){
         })
     };
 
-    const handleMouseEnter = () => {
+    const handleDblClick = () => {
+        setIsEditing(!isEditing);
+    }
+
+    const submitEdit = () => {
+        
+        dispatch({
+            type: 'edit',
+            payload: editedTitle.current.value
+        });
+        setIsEditing(!isEditing);
+    }
+
+    const handleMouseOver = () => {
         setHovering(true);
     }
     const handleMouseLeave = () => {
@@ -24,20 +39,31 @@ export default function TodoItem({item}){
     return(
         <li 
         className="todo-item-container" 
-        onMouseEnter={handleMouseEnter} 
+        onMouseOver={handleMouseOver} 
         onMouseLeave={handleMouseLeave}>
             <div className="checkbox-title-container" >
-                <div className={ `checkbox icon-container ${todo.checked? 'checkbox-checked': ''}`} onClick={handleChange} role="button" >
+                <div 
+                className={ `checkbox icon-container ${todo.checked? 'checkbox-checked': ''}`} 
+                onClick={handleChange} 
+                role="button" >
                 {
                     todo.checked ? <Icon.Checked /> : <Icon.Unchecked />
                 }
                 </div>
-                <span className={`todo-title ${todo.checked? 'title-checked' : ''}`}>
-                    {item.title}
-                </span>
+                {
+                    isEditing ? 
+                    <form onSubmit={submitEdit}>
+                        <input ref={editedTitle} type="text" />
+                    </form> 
+                    : 
+                    <div 
+                    className={`todo-title ${todo.checked? 'title-checked' : ''}`}
+                    onDoubleClick={handleDblClick}>
+                        {todo.title}
+                    </div>
+                }
             </div>
-            
-            <button className={`delete-todo icon-container`} onClick={()=>context.deleteTodo(item)}>
+            <button className={`delete-todo icon-container`} onClick={()=>context.deleteTodo(todo)}>
                 { hovering ? <Icon.Trashcan />: null}
             </button>
         </li>
@@ -52,7 +78,7 @@ const todoItemReducer = (state, action) =>{
             return {...state, checked:payload};
         }
         case 'edit':{
-            break;
+            return {...state, title:payload}
         }
         default: {
             break;
